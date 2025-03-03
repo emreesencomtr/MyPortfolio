90259ba1439010ae88e625421c86818101b5d9a9
Emre Esen <emreakkaya1905@gmail.com>
2025-03-03 23:41:33 +0300

    Add project files.

---
+++ MyPortfolio/Dal/Entities/SocialMedia.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPortfolio.Dal.Entities
+{
+    public class SocialMedia
+    {
+        [Key]
+        public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
