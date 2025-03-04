a1c92d23b8879c8b7ee473721d544dc44e5fea69
Emre Esen <emreakkaya1905@gmail.com>
2025-03-04 23:34:22 +0300

    04.03.2025

---
+++ MyPortfolio/ViewComponents/LayoutViewComponent/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponent
+{
+    public class _LayoutHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
