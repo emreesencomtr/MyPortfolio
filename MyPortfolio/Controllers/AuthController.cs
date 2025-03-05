8cd9a73a31b757f1a58db6b2c0768d9fbff02c31
Emre Esen <emreakkaya1905@gmail.com>
2025-03-05 13:43:02 +0300

    final

---
+++ MyPortfolio/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Data;
+
+namespace MyPortfolio.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            // FakeDatabase'deki kullanıcılara bakıyoruz
+            var user = FakeDatabase.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user != null)
+            {
+                // Giriş başarılıysa ExperienceList sayfasına yönlendir
+                return RedirectToAction("ExperienceList", "Experience");
+            }
+
+            // Hatalı giriş yapılırsa, hata mesajı göster
+            ViewBag.Error = "Hatalı e-posta veya şifre!";
+            return View();
+        }
+    }
+}
---
+++ MyPortfolio/Data/FakeDatabase.cs
@@ -0,0 +1,13 @@
+﻿using MyPortfolio.Models;
+
+namespace MyPortfolio.Data
+{
+    public static class FakeDatabase
+    {
+        public static List<User> Users = new List<User>
+    {
+        new User { Id = 1, Email = "admin@admin.com", Password = "1234" },
+        new User { Id = 2, Email = "admin@example.com", Password = "admin" }
+    };
+    }
+}
